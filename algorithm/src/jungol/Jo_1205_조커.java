7a2d1bab11a98388e249b686b4f7da1fb867de57
jabdev1009 <jabdev1009@gmail.com>
2025-01-29 03:15:22 +0900

    250129

--- algorithm/src/jungol/Jo_1205_조커.java
+++ algorithm/src/jungol/Jo_1205_조커.java
@@ -9,7 +9,7 @@ public class Jo_1205_조커 {
 		int n = sc.nextInt();
 		int[] nums = new int[n];
 		int countZoker = 0;
-		
+		// 조커의 수를 세고 배열에서 조커를 앞으로 몰아둠
 		for(int i=0; i<n; i++) {
 			nums[i] = sc.nextInt();
 			if(nums[i] == 0) {
@@ -20,24 +20,32 @@ public class Jo_1205_조커 {
 		
 		int straight = 0;
 		int sum = 1;
+		// 조커만 있는 경우 카드의 수만큼 스트레이트
 		if(countZoker == n) {
 			straight = n;
 		} else {
+			// 조커의 수를 idx로 하는 반복문
 			for(int i=countZoker; i<n; i++) {
 				int cnt = 0;
 				for(int j=0; j<n; j++) {
+					// 마지막 인덱스인 경우 남은 조커의 수를 합에 더하고 break
 					if(i+j == n-1) {
 						if(countZoker > cnt) {
 							sum += (countZoker - cnt);
 						}
 						break;
 					} else {
+						// 다음 인덱스에 있는 숫자와의 차이를 구한 뒤
+						// 차이보다 조커의 수가 적으면 남은 조커의 수를 합에 더하고 break
 						int gaps = nums[i+j+1] - nums[i+j] -1;
 						if(gaps > 0) {
 							if(gaps > countZoker - cnt) {
 								sum += (countZoker - cnt);
 								break;
 							} else {
+								// 차이보다 조커의 수가 많으면 사용한 조커수에 차이를 더한 뒤
+								// 차이를 더한 값이 조커 수보다 크면 break
+								// 아니면 차이를 합에 더하고 1을 더해줌
 								cnt += gaps;
 								if(countZoker < cnt) {
 									break;
--- algorithm/src/swea/Swea_1220_Magnetic.java
+++ algorithm/src/swea/Swea_1220_Magnetic.java
@@ -1,5 +1,37 @@
 package swea;
 
-public class Swea_1220_Magnetic {
+import java.util.Scanner;
 
+public class Swea_1220_Magnetic {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int test_case=0; test_case<10; test_case++) {
+			int[][] arr = new int[100][100];
+			int n = sc.nextInt();
+			for(int j=0; j<100; j++) {
+				for(int k=0; k<100; k++) {
+					arr[j][k] = sc.nextInt();
+				}
+			}
+			// 1이 몇번을 나오던 2가 나오면 +1
+			// 1-2의 순서쌍이 몇번 나오는지 순회
+			int count = 0;
+			for(int j=0; j<100; j++) {
+				boolean flag = false;
+				for(int k=0; k<100; k++) {
+					if(arr[k][j] == 1) {
+						flag = true;
+					} else if(arr[k][j] == 2){
+						if(flag) {
+							count++;
+							flag = false;
+						}
+					}
+				}
+			}
+			
+			System.out.println("#"+(test_case+1)+" "+count);
+		}
+	}
 }
