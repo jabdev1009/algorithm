30f300ec7bcc0f2f07f31a897819992e8c36ad2a
jabdev1009 <jabdev1009@gmail.com>
2025-02-11 13:42:48 +0900

    250211

---
+++ day02_0210/src/Swea_1208_Flatten.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Swea_1208_Flatten {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+		for(int tc = 1; tc<=T; tc++) {
+			int n = sc.nextInt();
+			int[] arr = new int[100];
+			for(int i=0; i<100; i++) {
+				arr[i] = sc.nextInt();
+			}
+					
+			Arrays.sort(arr);
+			
+			int min = arr[0]; 
+			int max = arr[99];
+			for(int i=0; i<n; i++) {
+				if(max - min > 1) {
+					arr[0]++;
+					arr[99]--;
+					Arrays.sort(arr);
+				} else {
+					break;
+				}
+			}
+			min = arr[0];
+			max = arr[99];
+			System.out.println("#"+tc+" " + (max - min));
+		}
+	}
+}
