1acaf572e7712170a8077fde56949bb5becce951
jabdev1009 <jabdev1009@gmail.com>
2025-02-22 00:16:39 +0900

    250222

---
+++ 250221/src/boj_11005_진법변환2/Main.java
@@ -0,0 +1,15 @@
+package boj_11005_진법변환2;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int n = sc.nextInt();
+		
+		while(num / n > 0) {
+			
+		}
+	}
+}
--- algorithm/src/StackTest.java
+++ algorithm/src/StackTest.java
@@ -2,17 +2,9 @@ import java.util.Stack;
 
 public class StackTest {
 	public static void main(String[] args) {
-		Stack<Integer> s = new Stack();
-		s.push(5);
-		s.push(2);
-		s.push(3);
-		s.push(7);
-		s.pop();
-		s.push(1);
-		s.push(4);
-		s.pop();
-		while(!s.empty()) {
-			System.out.println(s.pop());
-		}
+		double a = 100.1;
+		double b = 100.3;
+		
+		System.out.println(100.1+100.3 == 200.4);
 	}
 }
