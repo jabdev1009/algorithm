df31dfcf6662984827ced5a492e1223e276a959c
jabdev1009 <jabdev1009@gmail.com>
2025-03-06 18:51:33 +0900

    pull 전 커밋

--- 250304/src/Test.java
+++ 250304/src/Test.java
@@ -1,68 +1,39 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = Integer.parseInt(sc.nextLine());
-        
-        int tc = 1; 
-        while(tc<=T) {
-            StringBuilder sb = new StringBuilder();
-            int N = Integer.parseInt(sc.nextLine());
-            int[] tree = new int[N]; 
-            
-            int max = 0; 
-            String[] input = sc.nextLine().split(" ");
-            for(int i=0; i<N; i++) {
-                tree[i] = Integer.parseInt(input[i]);
-                max = Math.max(max, tree[i]);
-            }
-            
-            Stack<Integer> st1 = new Stack<>(); //gap을 2로 나눈 나머지인 1만 들어감
-            Stack<Integer> st2 = new Stack<>(); //gap을 2로 나눈 몫의 개수만큼 2가 들어감. 
-            
-            for(int i=0; i<N; i++) {
-                int gap = max-tree[i];
-
-                if(gap%2==1) st1.push(1);
-                int gap2 = gap / 2; 
-                for(int j=0; j<gap2; j++) st2.push(2);
-            }
-            
-            int dayCnt2 = 0; //2일 단위 데이카운트 
-            while(!st1.isEmpty() && !st2.isEmpty()) {
-                st1.pop();
-                st2.pop();
-                dayCnt2++;
-            }
-            
-            int dayCnt4 = 0; //4일 단위 데이카운트
-            if(!st2.isEmpty()) { //2가 남았으면 3개씩 짝지음 
-                while(st2.size()>2) {
-                    st2.pop();
-                    st2.pop();
-                    st2.pop();
-                    dayCnt4++;
-                }
-            }
-            
-            int dayCnt1 = 0; //1일 단위 데이카운트
-            if(!st2.isEmpty()) {
-                if(st2.size()==1) {
-                    dayCnt1 += 2;
-                } else if(st2.size()==2) {
-                    dayCnt1 += 3;
-                }                
-            } else if(!st1.isEmpty()) {  
-                dayCnt1 += (2*st1.size())-1; //끝까지 남은 하루단위들
-            }
-            
-            int result = dayCnt1+dayCnt2*2+dayCnt4*4;
-            sb.append("#"+tc+" "+result);
-            System.out.println(sb);
-                    
-            tc++;
-        }
-                
-    }
+public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	
+	int T = sc.nextInt();
+	
+	for (int tc = 1; tc <= T; tc++) {
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int [] arr = new int [N];
+		
+		for (int n = 0; n < N; n++) {
+			arr[n] = sc.nextInt();
+		}
+		
+		int sum = 1;
+		int max = 0;
+		
+		Arrays.sort(arr);
+		
+		for (int n = N-1; n > 1; n--) {
+			for (int i = 0; i < n; i++) {
+				if (arr[n]-arr[i] <= K) {
+					sum++;
+				}
+			}
+			max = Math.max(sum, max);
+			sum = 1;
+		}
+		
+		System.out.println("#" + tc + " " + max);
+		
+	}//tc
+}//main
 }
--- 250305/src/im_야구선수/Solution.java
+++ 250305/src/im_야구선수/Solution.java
@@ -14,12 +14,14 @@ public class Solution {
 			
 			int n = sc.nextInt();
 			int k = sc.nextInt();
-			
+			// 범위만큼 배열을 만들어줌
 			int[] arr = new int[1001];
 			
 			int min = Integer.MAX_VALUE;
 			int max = Integer.MIN_VALUE;
 			
+			// 이후 반복문을 돌 때 범위를 정해주기 위해서 최대, 최소값을 찾아주고
+			// 인덱스에 해당하는 값을 1증가 -> 카운트배열처럼
 			for(int i=0; i<n; i++) {
 				int num = sc.nextInt();
 				
@@ -34,19 +36,22 @@ public class Solution {
 			
 			
 			int sumMax = 0;
-			
+			// 숫자가 있는 곳부터 마지막 숫자가 있는 곳까지
 			for(int a=min; a<=max; a++) {
 				int sum = 0;
+				//차이가 k를 초과할 수 없기 때문에 시작 인덱스에서 k번째 인덱스까지만 더해줌
 				for(int i=0; i<=k; i++) {
+					// 인덱스 초과하면 안더해
 					if(a+i <= 1000) {
 						sum += arr[a+i];
 					}
 				}
+				// 지금 합이 현재 최대값보다 크니?
 				if(sum > sumMax) {
 					sumMax = sum;
 				}
 			}
-			
+			// 결과 붙여서 출력
 			sb.append(sumMax);
 			System.out.println(sb);
 			
